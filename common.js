737ac185053d4c708ea564620d86208f57841901
taoran7 <taoran7@xdf.cn>
2018-03-06 12:36:49 +0800

    debug

--- common.js
+++ common.js
@@ -1,3 +1,28 @@
-
-(function () { custnum.onclick = function () { iframe.contentWindow.document.querySelector('iframe').contentWindow.txtIdentify.disabled = false; iframe.contentWindow.document.querySelector('iframe').contentWindow.flagDate = true }; document.querySelector('body').addEventListener('click', function (e) { if (e.target.className == 'window_bg') { $('#camera iframe').each(function (index, obj) { $(obj.contentWindow.document).find('#file').show(); obj.contentWindow.flagidentitypicdoublestaff = 'N'; }); $('#addrDialog iframe').each(function (index, obj) { $(obj.contentWindow.document).find('#txtIdentify')[0].disabled = false; $(obj.contentWindow.document).find('#txtIdentify')[0].value = $(obj.contentWindow.document).find('#txtIdentify').attr('infosafedata'); $(obj.contentWindow.document).find('#custMailAddress')[0].value = $(obj.contentWindow.document).find('#custMailAddress').attr('infosafedata'); $(obj.contentWindow.document).find('#mailAddr')[0].value = $(obj.contentWindow.document).find('#mailAddr').attr('infosafedata'); }); } }); document.getElementById('btnQuery').addEventListener('click', function () { setTimeout(function () { document.getElementById('btnVerify').style.display = '' }, 1000) }) })()
-
+(function () {
+    custnum.onclick = function () {
+        iframe.contentWindow.document.querySelector('iframe').contentWindow.txtIdentify.disabled = false;
+        iframe.contentWindow.document.querySelector('iframe').contentWindow.flagDate = true
+    };
+    document.querySelector('body').addEventListener('click',
+        function (e) {
+            if (e.target.className == 'window_bg') {
+                $('#camera iframe').each(function (index, obj) {
+                    $(obj.contentWindow.document).find('#file').show();
+                    obj.contentWindow.flagidentitypicdoublestaff = 'N';
+                });
+                $('#addrDialog iframe').each(function (index, obj) {
+                    $(obj.contentWindow.document).find('#txtIdentify')[0].disabled = false;
+                    $(obj.contentWindow.document).find('#txtIdentify')[0].value = $(obj.contentWindow.document).find('#txtIdentify').attr('infosafedata') || $(obj.contentWindow.document).find('#txtIdentify')[0].value;
+                    $(obj.contentWindow.document).find('#custMailAddress')[0].value = $(obj.contentWindow.document).find('#custMailAddress').attr('infosafedata') || $(obj.contentWindow.document).find('#custMailAddress')[0].value;
+                    $(obj.contentWindow.document).find('#mailAddr')[0].value = $(obj.contentWindow.document).find('#mailAddr').attr('infosafedata') || $(obj.contentWindow.document).find('#mailAddr')[0].value;
+                });
+            }
+        });
+    document.getElementById('btnQuery').addEventListener('click',
+        function () {
+            setTimeout(function () {
+                document.getElementById('btnVerify').style.display = ''
+            },
+                1000)
+        })
+})()
