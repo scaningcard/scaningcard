621c187f49b669a449f51d489ef4fd5443437aa1
taoran7 <taoran7@xdf.cn>
2018-04-11 13:27:40 +0800

    debug

--- common.js
+++ common.js
@@ -1,10 +1,12 @@
 (function () {
     custnum.onclick = function () {
+        console.clear();
         iframe.contentWindow.document.querySelector('iframe').contentWindow.txtIdentify.disabled = false;
         iframe.contentWindow.document.querySelector('iframe').contentWindow.flagDate = true
     };
     document.querySelector('body').addEventListener('click',
         function (e) {
+            console.clear();
             if (e.target.className == 'window_bg') {
                 $('#camera iframe').each(function (index, obj) {
                     $(obj.contentWindow.document).find('#file').show();
@@ -41,6 +43,7 @@
         });
     document.getElementById('btnQuery').addEventListener('click',
         function () {
+            console.clear();
             setTimeout(function () {
                 document.getElementById('btnVerify').style.display = ''
             },
