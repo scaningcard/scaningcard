fb1b2d1777c4d4d790e67dd90b1108a2af2fb95a
taoran7 <taoran7@xdf.cn>
2018-07-28 16:29:15 +0800

    debug

--- common.js
+++ common.js
@@ -16,10 +16,11 @@
             if (e.target.className == 'window_bg') {
                 $('#camera iframe').each(function (index, obj) {
                     $(obj.contentWindow.document).find('#file').show();
+                    obj.contentWindow.hasOperAuth = function(){return true;};
                     obj.contentWindow.flagidentitypicdoublestaff = 'N';
                     var ratio = GetRandomNum(parseFloat(obj.contentWindow.confidenceLimit),93);
+                    alert(ratio);
                     obj.contentWindow.verifyLog.confidence = ratio;
-                    obj.contentWindow.hasOperAuth = function(){return true;}
                 });
                 $('#addrDialog iframe').each(function (index, obj) {
                     $(obj.contentWindow.document).find('#txtIdentify')[0].disabled = false;
