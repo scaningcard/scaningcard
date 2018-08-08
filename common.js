c461d5dacbac3fcc74268ae1a16e6dcc75ef0215
taoran7 <taoran7@xdf.cn>
2018-08-08 18:12:18 +0800

    debug

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     wScaleThreshold = 0.01;
     hScaleThreshold = 0.01;
     // console.clear();
@@ -6,62 +6,193 @@
     //     iframe.contentWindow.console.clear();
     // },1000);
     // console.log = function(){};
-    
-    custnum.onclick = function () {
+
+    custnum.onclick = function() {
         iframe.contentWindow.document.querySelector('iframe').contentWindow.txtIdentify.disabled = false;
-        iframe.contentWindow.document.querySelector('iframe').contentWindow.flagDate = true
+        iframe.contentWindow.document.querySelector('iframe').contentWindow.flagDate = true;
     };
-    document.querySelector('body').addEventListener('click',
-        function (e) {
-            if (e.target.className == 'window_bg') {
-                $('#camera iframe').each(function (index, obj) {
-                    $(obj.contentWindow.document).find('#file').show();
-                    obj.contentWindow.hasOperAuth = function(){return true;};
-                    obj.contentWindow.flagidentitypicdoublestaff = 'N';
-                    var ratio = GetRandomNum(parseFloat(obj.contentWindow.confidenceLimit),93);
-                    alert(ratio);
-                    obj.contentWindow.verifyLog.confidence = ratio;
-                });
-                $('#addrDialog iframe').each(function (index, obj) {
-                    $(obj.contentWindow.document).find('#txtIdentify')[0].disabled = false;
-                    $(obj.contentWindow.document).find('#txtIdentify')[0].value = $(obj.contentWindow.document).find('#txtIdentify').attr('infosafedata') || $(obj.contentWindow.document).find('#txtIdentify')[0].value;
-                    $(obj.contentWindow.document).find('#custMailAddress')[0].value = $(obj.contentWindow.document).find('#custMailAddress').attr('infosafedata') || $(obj.contentWindow.document).find('#custMailAddress')[0].value;
-                    $(obj.contentWindow.document).find('#mailAddr')[0].value = $(obj.contentWindow.document).find('#mailAddr').attr('infosafedata') || $(obj.contentWindow.document).find('#mailAddr')[0].value;
-                    // obj.contentWindow.g_isAddCustomer = true;
-                    obj.contentWindow.InstInfoSafe.clearEncode = function () { };
-                });
-            } else if (e.target.className == 'head') {
-                if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operator')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operator').removeAttribute('readonly');
+    document.querySelector('body').addEventListener('click', function(e) {
+        if (e.target.className == 'window_bg') {
+            $('#camera iframe').each(function(index, obj) {
+                $(obj.contentWindow.document)
+                    .find('#file')
+                    .show();
+                obj.contentWindow.hasOperAuth = function() {
+                    return true;
+                };
+                obj.contentWindow.flagidentitypicdoublestaff = 'N';
+                var ratio = GetRandomNum(parseFloat(obj.contentWindow.confidenceLimit), 93);
+                alert(ratio);
+                obj.contentWindow.verifyLog.confidence = ratio;
+            });
+            $('#addrDialog iframe').each(function(index, obj) {
+                // $(obj.contentWindow.document).find('#txtIdentify')[0].disabled = false;
+                // $(obj.contentWindow.document).find('#txtIdentify')[0].value = $(obj.contentWindow.document).find('#txtIdentify').attr('infosafedata') || $(obj.contentWindow.document).find('#txtIdentify')[0].value;
+                // $(obj.contentWindow.document).find('#custMailAddress')[0].value = $(obj.contentWindow.document).find('#custMailAddress').attr('infosafedata') || $(obj.contentWindow.document).find('#custMailAddress')[0].value;
+                // $(obj.contentWindow.document).find('#mailAddr')[0].value = $(obj.contentWindow.document).find('#mailAddr').attr('infosafedata') || $(obj.contentWindow.document).find('#mailAddr')[0].value;
+                // obj.contentWindow.InstInfoSafe.clearEncode = function () { };
+
+                if (!g_isAddCustomer) {
+                    //脱敏修改,如果是修改客户,还原成原来的值
+                    InstInfoSafe.delInfoSafeData($('#txtIdentify'));
+                    InstInfoSafe.delInfoSafeData($('#txtCustName'));
+                    InstInfoSafe.delInfoSafeData($('#custMailAddress'));
+                    InstInfoSafe.delInfoSafeData($('#mailAddr'));
                 }
-                if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorCard')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorCard').removeAttribute('readonly');
+                // PC获取证件姓名、证件地址、身份证号
+                var rtnObj = JSON.parse($('#txtCustName').val());
+                if (rtnObj.flag) {
+                    global.isComputerIdCard = true;
+                    getMainTop().isScanFlag = true;
+                    $('#txtCustName').val(rtnObj.name);
+                    $('#txtIdentify').val(rtnObj.cardNo);
+                    if (rtnObj.address.indexOf('对面') > -1) {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '旁') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '旁边') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '附近') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '路口') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '县') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '乡') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '区') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '镇') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '酒店') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '宾馆') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '公司') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 2) == '隔壁') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 3) == '交汇处') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '米') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 3) == '专卖店') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '箱') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '盒') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    } else if (rtnObj.address.substring(rtnObj.address.length - 1) == '上') {
+                        rtnObj.address = rtnObj.address + '(证件地址)';
+                    }
+
+                    $('#custMailAddress').val(rtnObj.address);
+                    $('#mailAddr').val(rtnObj.address);
+                    //置入读卡数据
+                    addCust.cardInfo.image = rtnObj.identityPic;
+                    addCust.cardInfo.name = rtnObj.name;
+                    addCust.cardInfo.sex = rtnObj.cust_sex;
+                    addCust.cardInfo.nation = rtnObj.nation;
+                    addCust.cardInfo.born = rtnObj.cust_birth;
+                    addCust.cardInfo.address = rtnObj.address;
+                    addCust.cardInfo.police = rtnObj.issuedauthority;
+                    addCust.cardInfo.num = rtnObj.cardNo;
+                    addCust.cardInfo.image = rtnObj.identityPic;
+                    $('#txtCustName').blur();
+                    $('#txtIdentify').blur();
+                    $('#custMailAddress').blur();
+                    $('#mailAddr').blur();
+                    //saveLog.saveScanIdentityLog("新增修改客户",1,1);
+                    saveLog.saveSDScanIdentityLog(
+                        rtnObj.name,
+                        rtnObj.cardNo,
+                        rtnObj.address,
+                        rtnObj.identityPic,
+                        rtnObj.cust_sex,
+                        rtnObj.nation,
+                        rtnObj.cust_birth,
+                        rtnObj.issuedauthority,
+                        rtnObj.validdateStart,
+                        rtnObj.validdateEnd,
+                        '1'
+                    );
+                    saveLog.saveLogCheckGzt(
+                        rtnObj.name,
+                        rtnObj.cardNo,
+                        rtnObj.address,
+                        rtnObj.identityPic,
+                        rtnObj.cust_sex,
+                        rtnObj.nation,
+                        rtnObj.cust_birth,
+                        rtnObj.issuedauthority,
+                        rtnObj.validdateStart,
+                        rtnObj.validdateEnd,
+                        '1'
+                    );
+                    //给客户照片对象赋值
+                    if (addCust && addCust.cardInfo.image && addCust.cardInfo.image != '') {
+                        $('#custIdentifyImg').attr('src', 'data:image/png;base64,' + addCust.cardInfo.image);
+                    }
+                } else {
+                    $('#txtCustName').val('');
+                    $('#txtIdentify').val('');
+                    $('#custMailAddress').val('');
+                    $('#mailAddr').val('');
                 }
-                if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorAddr')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorAddr').removeAttribute('readonly');
-                }
-                if (document.getElementById('iframe').contentWindow.document.getElementById('agentManInput')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('agentManInput').removeAttribute('readonly');
-                }
-                if (document.getElementById('iframe').contentWindow.document.getElementById('agentManIdentityInput')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('agentManIdentityInput').removeAttribute('readonly');
-                }
-                if (document.getElementById('iframe').contentWindow.document.getElementById('agentManAddressInput')) {
-                    document.getElementById('iframe').contentWindow.document.getElementById('agentManAddressInput').removeAttribute('readonly');
-                }
+                $('#txtCustName').attr('readonly', 'readonly');
+                $('#txtIdentify').attr('readonly', 'readonly');
+                $('#custMailAddress').attr('readonly', 'readonly');
+                $('#btnValidate').hide();
+                addCust.qryType = 2;
+            });
+        } else if (e.target.className == 'head') {
+            if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operator')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('boManagerAttrs_operator')
+                    .removeAttribute('readonly');
             }
-        });
-    document.getElementById('btnQuery').addEventListener('click',
-        function () {
-            setTimeout(function () {
-                document.getElementById('btnVerify').style.display = ''
-            },
-                1000)
-        });
+            if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorCard')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('boManagerAttrs_operatorCard')
+                    .removeAttribute('readonly');
+            }
+            if (document.getElementById('iframe').contentWindow.document.getElementById('boManagerAttrs_operatorAddr')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('boManagerAttrs_operatorAddr')
+                    .removeAttribute('readonly');
+            }
+            if (document.getElementById('iframe').contentWindow.document.getElementById('agentManInput')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('agentManInput')
+                    .removeAttribute('readonly');
+            }
+            if (document.getElementById('iframe').contentWindow.document.getElementById('agentManIdentityInput')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('agentManIdentityInput')
+                    .removeAttribute('readonly');
+            }
+            if (document.getElementById('iframe').contentWindow.document.getElementById('agentManAddressInput')) {
+                document
+                    .getElementById('iframe')
+                    .contentWindow.document.getElementById('agentManAddressInput')
+                    .removeAttribute('readonly');
+            }
+        }
+    });
+    document.getElementById('btnQuery').addEventListener('click', function() {
+        setTimeout(function() {
+            document.getElementById('btnVerify').style.display = '';
+        }, 1000);
+    });
 
     function GetRandomNum(Min, Max) {
         var Range = Max - Min;
         var Rand = Math.random();
-        return(Min + (Rand * Range)).toFixed(6);
+        return (Min + Rand * Range).toFixed(6);
     }
 })();
